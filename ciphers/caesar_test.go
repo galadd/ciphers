8e75a6ca4b4b6e3ba9a7a6e9cf6d7316bbfc9da5
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2023-01-05 18:54:00 +0100

    test: caesar encryption

---
+++ ciphers/caesar_test.go
@@ -0,0 +1,42 @@
+package caesar
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncrypt(t *testing.T) {
+	tests := []struct {
+		name string
+		input string
+		expectedOutput string
+	}{
+		{
+			name: "Encrypt lowercase",
+			input: "abc",
+			expectedOutput: "def",
+		},
+		{
+			name: "Encrypt uppercase",
+			input: "ABC",
+			expectedOutput: "DEF",
+		},
+		{
+			name: "Encrypt mixed case",
+			input: "aBc",
+			expectedOutput: "dEf",
+		},
+		// {
+		// 	name: "Encrypt special characters",
+		// 	input: "!@#$%^&*()",
+		// 	expectedOutput: "!@#$%^&*()",
+		// },
+	}
+	
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			output := Encrypt(test.input)
+			assert.Equal(t, test.expectedOutput, output)
+		})
+	}
+}
