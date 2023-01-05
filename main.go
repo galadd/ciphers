a22f3e44a4b45e5a0931422b81bf7193fce2cceb
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2023-01-05 20:18:16 +0100

    cmd: add caesar encryption

--- main.go
+++ main.go
@@ -9,6 +9,37 @@ import (
 )
 
 func main() {
-	encryption := caesar.Encrypt("hello")
-	fmt.Println(encryption)
+	var ed int
+	fmt.Println("Enter 1 to Encrypt or 2 to Decrypt")
+	fmt.Scanln(&ed)
+
+	var choice int
+	fmt.Println("Choose a cipher:")
+	fmt.Println("1. Caesar")
+	fmt.Scanln(&choice)
+
+	var message string
+	fmt.Println("Write your message:")
+	fmt.Scanln(&message)
+
+	var codec string
+
+	switch choice {
+	case 1: 
+	if ed == 1 {
+		codec = caesar.Encrypt(message)
+	}
+	if ed == 2 {
+		codec = caesar.Decrypt(message)
+	}
+	}
+	fmt.Println()
+	fmt.Println("⬇️  ⬇️  ⬇️  ⬇️  ⬇️")
+
+	if ed == 1 {
+		fmt.Printf("Message Encrypted: %s\n", codec)
+	}
+	if ed == 2 {
+		fmt.Println("Message Decrypted: %s\n", codec)
+	}
 }
