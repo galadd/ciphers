f261765cf96523aad4ffeafc0a3e5d9d6f9c5807
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2023-01-06 02:51:40 +0100

    Add caesar cipher

---
+++ ciphers/caesar/caesar.go
@@ -0,0 +1,49 @@
+package caesar
+
+func Encrypt(message string) string {
+	var encrypted string
+	
+	// Create a lookup table for the encrypted characters
+	lookup := make(map[rune]rune)
+	for i := 'a'; i <= 'z'; i++ {
+		lookup[i] = rune((int(i - 'a') + 3) % 26 + 'a')
+	}
+	for i := 'A'; i <= 'Z'; i++ {
+		lookup[i] = rune((int(i - 'A') + 3) % 26 + 'A')
+	}
+	
+	// Use the lookup table to encrypt the input string
+	for _, char := range message {
+		if lookup[char] == 0 {
+			encrypted += string(char)
+			continue
+		}
+		encrypted += string(lookup[char])
+	}
+	
+	return encrypted
+}
+
+func Decrypt(message string) string {
+	var decrypted string
+	
+	// Create a lookup table for the decrypted characters
+	lookup := make(map[rune]rune)
+	for i := 'a'; i <= 'z'; i++ {
+		lookup[i] = rune((int(i - 'a') - 3) % 26 + 'a')
+	}
+	for i := 'A'; i <= 'Z'; i++ {
+		lookup[i] = rune((int(i - 'A') - 3) % 26 + 'A')
+	}
+	
+	// Use the lookup table to decrypt the input string
+	for _, char := range message {
+		if lookup[char] == 0 {
+			decrypted += string(char)
+			continue
+		}
+		decrypted += string(lookup[char])
+	}
+	
+	return decrypted
+}
